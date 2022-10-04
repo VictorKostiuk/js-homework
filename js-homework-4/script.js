24a60eb4d7dc83a1db5920c41d657510fefac5db
Viktor Kostiuk <vityok.kostiuk@gmail.com>
2022-10-05 00:08:33 +0300

    All tasks done for hw 4

---
+++ js-homework-4/script.js
@@ -0,0 +1,72 @@
+// let num = +prompt('Enter number')
+// i = 0
+// do {
+//     document.write('#')
+//     i++
+// } while(i !== num)
+//
+// function factorial(num){
+//     if(num === 0 || num === 1){
+//         return 1
+//     }else{
+//         return num * factorial(num-1)
+//     }
+// }
+// alert("The factorial of " + num + " is " + factorial(num))
+
+// Завдання 2
+
+// let answer = +prompt('Solve this example 2+2*2')
+//
+// if(answer !== 6) {
+//     do {
+//         answer = +prompt('Try again!')
+//     } while(answer !== 6)
+// } else {
+//     alert('Good job!')
+// }
+
+// Завдання 3
+
+// for (let i = 0; i <= 100; i++) {
+//     document.write(i)
+// }
+
+// let num = +prompt('Enter number')
+//
+// const isPrime = num => {
+//     for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+//         if(num % i === 0) return false;
+//     return num > 1;
+// }
+//
+// alert(isPrime(num))
+
+// Завдання 4
+
+// n = +prompt('Enter first number')
+// m = +prompt('Enter second number')
+// k = +prompt('Enter number')
+// document.write(k)
+// for(let i = n; i <= m; i++) {
+//     document.write(i)
+// }
+
+// Завдання 5
+
+// n = +prompt('Enter number from 1 to 9')
+// i = 0
+//
+// while(i <= 10) {
+//     document.write(n + ' * ' + i + ' = ' + (n * i) + '; ')
+//     i++
+// }
+
+// Завдання 6
+
+// let sum = 0
+//
+// for(let i = 0; i !== 1000; i++) {
+//     sum += i
+// }
+// alert(sum/999)
